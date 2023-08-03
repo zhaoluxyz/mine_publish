cf2401733b0cde69b6767293779866b54191af2f
zhaozhenguo <zhaozhenguo@tuyoogame.com>
2023-08-03 18:50:28 +0800

    自动发布版本 0

---
+++ web-desktop/index.17fe0.js
@@ -0,0 +1,25 @@
+System.register(["./application.bda40.js"], function (_export, _context) {
+  "use strict";
+
+  var Application, application;
+
+  function topLevelImport(url) {
+    return System["import"](url);
+  }
+
+  return {
+    setters: [function (_applicationBda40Js) {
+      Application = _applicationBda40Js.Application;
+    }],
+    execute: function () {
+      application = new Application();
+      topLevelImport('cc').then(function (engine) {
+        return application.init(engine);
+      }).then(function () {
+        return application.start();
+      })["catch"](function (err) {
+        console.error(err);
+      });
+    }
+  };
+});
