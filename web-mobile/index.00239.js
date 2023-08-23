6d6641a79924ef35ea7e058379eb77995db931a0
zhaozhenguo <zhaozhenguo@tuyoogame.com>
2023-08-23 09:59:57 +0800

    自动发布版本 9

--- web-mobile/index.00239.js
+++ web-mobile/index.00239.js
@@ -1,4 +1,4 @@
-System.register(["./application.4ed76.js"], function (_export, _context) {
+System.register(["./application.7ff0a.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.4ed76.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application4ed76Js) {
-      Application = _application4ed76Js.Application;
+    setters: [function (_application7ff0aJs) {
+      Application = _application7ff0aJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
